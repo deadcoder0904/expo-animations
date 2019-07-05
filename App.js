797d75c1466b6e90e81c21a477a6576f037d6deb
Akshay Kadam <akshaykadam0904@gmail.com>
2019-07-05 20:44:15 +0530

    Parallel

--- App.js
+++ App.js
@@ -1,35 +1,82 @@
 import React from 'react'
-import { Animated, StyleSheet, View } from 'react-native'
+import { Animated, Button, Easing, StyleSheet, Text, View } from 'react-native'
 
 class App extends React.Component {
   constructor() {
     super()
-    this.springValue = new Animated.Value(0.3)
+    this.opacityValue = new Animated.Value(0)
+    this.translateValue = new Animated.Value(0)
+    this.scaleValue = new Animated.Value(0)
   }
 
   componentDidMount() {
-    this.spring()
+    this.animate()
   }
 
-  spring() {
-    this.springValue.setValue(0.3)
-    Animated.spring(this.springValue, {
-      toValue: 1,
-      friction: 1,
-    }).start(() => this.spring())
+  animate() {
+    this.opacityValue.setValue(0)
+    this.translateValue.setValue(0)
+    this.scaleValue.setValue(0)
+
+    Animated.parallel([
+      Animated.timing(this.opacityValue, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.ease,
+      }),
+      Animated.timing(this.translateValue, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.ease,
+        delay: 1000,
+      }),
+      Animated.timing(this.scaleValue, {
+        toValue: 1,
+        friction: 1,
+        delay: 2000,
+      }),
+    ]).start()
   }
 
   render() {
+    const opacity = this.opacityValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [1, 0, 1],
+    })
+    const translate = this.translateValue.interpolate({
+      inputRange: [0, 0.2, 0.4, 0.6, 0.8, 1],
+      outputRange: [0, 50, 100, -50, -100, 0],
+    })
+    const scale = this.scaleValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0.5, 3],
+    })
     return (
       <View style={styles.container}>
-        <Animated.Image
+        <Animated.View
           style={{
-            width: 227,
-            height: 200,
-            transform: [{ scale: this.springValue }],
+            opacity,
           }}
-          source={require('./react-native-logo.png')}
-        />
+        >
+          <Text style={styles.text}>Opaque Animation</Text>
+        </Animated.View>
+        <Animated.View
+          style={{
+            transform: [{ translateX: translate }],
+          }}
+        >
+          <Text style={styles.text}>Translate Animation</Text>
+        </Animated.View>
+        <Animated.View
+          style={{
+            transform: [{ scale: this.scaleValue }],
+          }}
+        >
+          <Text style={styles.text}>Spring Animation</Text>
+        </Animated.View>
+        <View style={styles.btnContainer}>
+          <Button color="tomato" title="Restart Animation" onPress={() => this.animate()} />
+        </View>
       </View>
     )
   }
@@ -41,6 +88,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+  },
+  btnContainer: {
+    marginVertical: 20,
   },
 })
 
