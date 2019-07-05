04e392caa8d2331b0f635eed3b50a51817fcf368
Akshay Kadam <akshaykadam0904@gmail.com>
2019-07-05 15:50:17 +0530

    Infinite Animation

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Animated, Easing, StyleSheet, View } from 'react-native'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.spinValue = new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    this.spin()
+  }
+
+  spin() {
+    this.spinValue.setValue(0)
+    Animated.timing(this.spinValue, {
+      toValue: 1,
+      duration: 4000,
+      easing: Easing.linear,
+    }).start(() => this.spin())
+  }
+
   render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    })
     return (
       <View style={styles.container}>
-        <Text>Animations!</Text>
+        <Animated.Image
+          style={{
+            width: 227,
+            height: 200,
+            transform: [{ rotate: spin }],
+          }}
+          source={require('./react-native-logo.png')}
+        />
       </View>
     )
   }
