c29a67ae28e7fda3fa3960277133f15febdaffac
Akshay Kadam <akshaykadam0904@gmail.com>
2019-07-06 13:04:45 +0530

    Stagger

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Animated, StatusBar, StyleSheet, View } from 'react-native'
+
+const arr = Array.from(Array(500).keys())
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.animatedValue = []
+    arr.forEach(value => {
+      this.animatedValue[value] = new Animated.Value(0)
+    })
+  }
+
+  componentDidMount() {
+    this.animate()
+  }
+
+  animate() {
+    Animated.stagger(
+      100,
+      arr.map(item => {
+        return Animated.timing(this.animatedValue[item], {
+          toValue: 1,
+          duration: 50,
+        })
+      }),
+    ).start()
+  }
+
   render() {
+    const animations = arr.map((a, i) => {
+      return (
+        <Animated.View
+          key={i}
+          style={{
+            opacity: this.animatedValue[a],
+            height: 20,
+            width: 20,
+            backgroundColor: 'tomato',
+            margin: 2,
+          }}
+        />
+      )
+    })
     return (
       <View style={styles.container}>
-        <Text>Animations!</Text>
+        <StatusBar hidden />
+        {animations}
       </View>
     )
   }
@@ -14,9 +55,9 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 })
 
